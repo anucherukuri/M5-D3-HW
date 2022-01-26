d3979f5aa19199ad9d6427341ebd1f4d954fd2e0
Anudeep <36949642+anucherukuri@users.noreply.github.com>
2022-01-26 16:31:56 +0000

    backend

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import express from "express" 
 import authorsRouter from "./services/authors/index.js"
+import blogPostsRouter from "./services/blogPosts/index.js"
+
+import {badRequestHandler,unauthorizedHandler,notFoundHandler,genericErrorHandler} from "./errorHandlers.js"
 const server = express()
 const port = 3003
 
 server.use(express.json())
 server.use("/authors", authorsRouter)
+server.use("/blogPost", blogPostsRouter)
+
+  // eroro middle wares
+server.use(badRequestHandler)
+server.use(unauthorizedHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
 server.listen(port, ()=>{
     console.log(`Server Running on port ${port}`)
 })
